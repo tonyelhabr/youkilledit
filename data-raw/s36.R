ed5f4a2694d3712cb8bbdea8a34dc4db50e4600a
Tony ElHabr <anthonyelhabr@gmail.com>
2021-06-22 08:30:58 -0500

    add s35 and s36 data

--- data-raw/s36.R
+++ data-raw/s36.R
@@ -1,66 +1,109 @@
 
-url <- wiki_links$url[36]
-page <- url %>% xml2::read_html()
+library(tidyverse)
+library(rvest)
+data('wiki_links', package = 'youkilledit')
 
-cells <-
-  page %>%
-  html_elements('.wikitable') %>%
-  pluck(4) %>%
-  html_elements('td')
-cells
-txt_trim <- cells %>% html_text(trim = TRUE)
-i <- cells %>% html_elements('i') %>% html_text(trim = TRUE) # %>% str_trim()
-i
-bg <- cells %>% html_attr('bgcolor')
-bg
-attrs <- cells %>% html_attrs()
-attrs
-nodes_colors <-
-  page %>%
-  html_elements('dl > dd > span')
-nodes_colors %>% html_elements('span')
-nodes_colors %>%
-  html_attr('style')
+retrieve_elims <-
+  function(season,
+           path_elims = file.path('data-raw', sprintf('elims-s%02d.rds', season)),
+           path_html = file.path('data-raw', sprintf('%02d.html', season)),
+           overwrite = FALSE) {
 
+    if(file.exists(path_elims) & !overwrite) {
+      return(path_elims)
+    }
 
-page %>%
-  html_elements('.wikitable') %>%
-  pluck(4) %>%
-  html_children() %>%
-  html_children()
+    if(!file.exists(path_html) | overwrite) {
+      url <- wiki_links %>% filter(order == !!season) %>% pull(url)
+      page <- url %>% xml2::read_html()
+      xml2::write_xml(page, path_html)
+    } else {
+      page <- path_html %>% xml2::read_html()
+    }
 
-df_init <-
-  page %>%
-  html_elements('.wikitable') %>%
-  pluck(4) %>%
-  html_table(header = FALSE) %>%
-  janitor::remove_empty() %>%
-  select(-1)
-df_init
-eps <-
-  df_init %>%
-  select(-1) %>%
-  slice(2) %>%
-  t() %>%
-  c() %>%
-  str_remove('\\[.*\\]')
-eps
+    colors <-
+      page %>%
+      html_elements('dl > dd > span') %>%
+      html_attr('style') %>%
+      as.character() %>%
+      str_replace('(background[-]color[:])(.*)([;]\\s?color.*$)', '\\2')
+    colors
 
-df_init %>%
-  slice(3:n()) %>%
-  set_names(c('contestant', eps)) %>%
-  pivot_longer(
-    -contestant,
-    names_to = 'ep',
-    values_to = 'value'
-  )
+    labs <-
+      page %>%
+      html_elements('dl > dd') %>%
+      html_text(trim = TRUE)
+    labs
 
-wiki_links %>%
-  mutate(
-    page = map(url, xml2::read_html)
-  ) %>%
-  mutate(
-    data = map(page, ~possibly(rvest::html_table(.x, fill = TRUE), otherwise = NULL))
-  ) %>%
-  select(-page)
-tables
+    color_labs <-
+      tibble(
+        # No Yellow, just yellow
+        color = tolower(colors),
+        lab = labs[1:length(colors)]
+      ) %>%
+      # some colors might be from a lower legend, so just the first.
+      group_by(color) %>%
+      slice(1) %>%
+      ungroup()
+    color_labs
+
+    tds <-
+      page %>%
+      html_elements('.wikitable') %>%
+      pluck(4) %>%
+      html_nodes('td')
+    tds
+
+    supp <-
+      tibble(
+        value = tds %>% html_text(trim = TRUE),
+        color = tds %>% html_attr('bgcolor')
+      ) %>%
+      mutate(contestant = ifelse(lag(color, 1) == 'black', value, NA_character_)) %>%
+      filter(value != '') %>%
+      fill(contestant) %>%
+      filter(value != contestant)
+    supp
+
+    df_init <-
+      page %>%
+      html_elements('.wikitable') %>%
+      pluck(4) %>%
+      html_table(header = FALSE) %>%
+      janitor::remove_empty(which = c('rows', 'cols')) %>%
+      select(-1)
+    df_init
+
+    eps <-
+      df_init %>%
+      select(-1) %>%
+      slice(2) %>%
+      t() %>%
+      c() %>%
+      str_remove('\\[.*\\]')
+    eps
+
+    df <-
+      df_init %>%
+      slice(3:n()) %>%
+      set_names(c('contestant', eps)) %>%
+      pivot_longer(
+        -contestant,
+        names_to = 'ep',
+        values_to = 'value'
+      ) %>%
+      filter(value != '') %>%
+      mutate(idx = row_number()) %>%
+      relocate(idx)
+    df
+    assertthat::assert_that(nrow(df) == nrow(supp), msg = '`df` and `supp` must have same number of rows')
+    res <-
+      df %>%
+      bind_cols(supp %>% select(color)) %>%
+      left_join(color_labs)
+    res
+    write_rds(res, path_elims)
+    path_elims
+  }
+
+35:36 %>% map_chr(retrieve_elims)
